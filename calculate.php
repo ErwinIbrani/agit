41cdb493cf16f70584d04cecf8e8e39c0859631a
Erwin <erwinlocalhost90@gmail.com>
2020-01-09 14:19:10 +0700

    'sdahup'

--- calculate.php
+++ calculate.php
@@ -4,15 +4,15 @@
 <title>Generate</title>
 <script type="text/javascript">
 function generate() {
-  var assign_date                  = (document.getElementById("assign_date").value);
+  var assigned_date_id             = (document.getElementById("assigned_date_id").value);
   var priority                     = (document.getElementById("priority").value);
-  var rca_status                   = (document.getElementById("rca_status").value);
-  var actual_rca_creation_date     = (document.getElementById("actual_rca_creation_date").value);
+  var rca_status                   = ((document.getElementById("rca_status")||{}).value)||"";
+  var actual_rca_date_id     = (document.getElementById("actual_rca_date_id").value);
   //auto RCA
-  var rebaseline_rca_creation_date = (document.getElementById("rebaseline_rca_creation_date").value);
+  var rebase_rca_create_target_id  = (document.getElementById("rebase_rca_create_target_id").value);
   //auto IMP
-  var rebaseline_imp_creation_date = (document.getElementById("rebaseline_imp_creation_date").value);
-  var date  = new Date(assign_date);
+  var rebase_imp_date_target_id = (document.getElementById("rebase_imp_date_target_id").value);
+  var date  = new Date(assigned_date_id);
 
     if(priority == 'P1')
     {
@@ -24,7 +24,7 @@ function generate() {
        var mm = newdate.getMonth() + 1;
        var y = newdate.getFullYear();
        var rcaCreationDate = y + '-' + mm + '-' + dd;
-       var rca_creation_date = document.getElementById('rca_creation_date').value = rcaCreationDate;
+       var rca_create_target_id = document.getElementById('rca_create_target_id').value = rcaCreationDate;
       
        var newdates     = new Date(date); 
        newdates.setDate(newdates.getDate() + w2);
@@ -32,7 +32,7 @@ function generate() {
        var mms = newdates.getMonth() + 1;
        var ys = newdates.getFullYear();
        var impCreationDate = ys + '-' + mms + '-' + dds;
-       var imp_creation_date = document.getElementById('imp_creation_date').value = impCreationDate;
+       var first_date_target_id = document.getElementById('first_date_target_id').value = impCreationDate;
     }
     
     else if(priority == 'P2')
@@ -44,7 +44,7 @@ function generate() {
        var mm = newdate.getMonth() + 1;
        var y = newdate.getFullYear();
        var rcaCreationDate = y + '-' + mm + '-' + dd;
-       var rca_creation_date = document.getElementById('rca_creation_date').value = rcaCreationDate;
+       var rca_create_target_id = document.getElementById('rca_create_target_id').value = rcaCreationDate;
       
        var w2 = 56; 
        var newdates     = new Date(date); 
@@ -53,7 +53,7 @@ function generate() {
        var mms = newdates.getMonth() + 1;
        var ys = newdates.getFullYear();
        var impCreationDate = ys + '-' + mms + '-' + dds;
-       var imp_creation_date = document.getElementById('imp_creation_date').value = impCreationDate;
+       var first_date_target_id = document.getElementById('first_date_target_id').value = impCreationDate;
     } 
     
     else if(priority == 'P3')
@@ -65,7 +65,7 @@ function generate() {
        var mm = newdate.getMonth() + 1;
        var y = newdate.getFullYear();
        var rcaCreationDate = y + '-' + mm + '-' + dd;
-       var rca_creation_date = document.getElementById('rca_creation_date').value = rcaCreationDate;
+       var rca_create_target_id = document.getElementById('rca_create_target_id').value = rcaCreationDate;
        
        var w2 = 84;
        var newdates     = new Date(date); 
@@ -74,7 +74,7 @@ function generate() {
        var mms = newdates.getMonth() + 1;
        var ys = newdates.getFullYear();
        var impCreationDate = ys + '-' + mms + '-' + dds;
-       var imp_creation_date = document.getElementById('imp_creation_date').value = impCreationDate;
+       var first_date_target_id = document.getElementById('first_date_target_id').value = impCreationDate;
     }
     
     else if(priority == 'P4')
@@ -86,7 +86,7 @@ function generate() {
        var mm = newdate.getMonth() + 1;
        var y = newdate.getFullYear();
        var rcaCreationDate = y + '-' + mm + '-' + dd;
-       var rca_creation_date = document.getElementById('rca_creation_date').value = rcaCreationDate;
+       var rca_create_target_id = document.getElementById('rca_create_target_id').value = rcaCreationDate;
        
        var w2 = 112;
        var newdates     = new Date(date); 
@@ -95,33 +95,33 @@ function generate() {
        var mms = newdates.getMonth() + 1;
        var ys = newdates.getFullYear();
        var impCreationDate = ys + '-' + mms + '-' + dds;
-       var imp_creation_date = document.getElementById('imp_creation_date').value = impCreationDate;
+       var first_date_target_id = document.getElementById('first_date_target_id').value = impCreationDate;
     }
   
   //RCA  
-  var _of_rebaselined_rca_creation_target = (document.getElementById("_of_rebaselined_rca_creation_target").value);
+  var num_rebase_create_target_id = (document.getElementById("num_rebase_create_target_id").value);
   var date1  = Date.now(); 
-  var date2  = Date.parse(rca_creation_date);
-  var dates2 = Date.parse(rebaseline_rca_creation_date); 
+  var date2  = Date.parse(rca_create_target_id);
+  var dates2 = Date.parse(rebase_rca_create_target_id); 
   
-  if(rebaseline_rca_creation_date === ""){  
+  if(rebase_rca_create_target_id === ""){  
     if(rca_status != 'Created')
     {
-      if(actual_rca_creation_date === "")
+      if(actual_rca_date_id === "")
       {
          if(date1 > date2)
          {
-            if(_of_rebaselined_rca_creation_target < 1)
+            if(num_rebase_create_target_id < 1)
             {
-                var addOneRCA  = _of_rebaselined_rca_creation_target + 1;   
+                var addOneRCA  = num_rebase_create_target_id + 1;   
                 var convertProcces = new Date(date2);
                 convertProcces.setDate(convertProcces.getDate() + w1);
                 var dd = convertProcces.getDate();
                 var mm = convertProcces.getMonth() + 1;
                 var y = convertProcces.getFullYear();
                 var conertSucces1 = y + '-' + mm + '-' + dd;
-                document.getElementById('rebaseline_rca_creation_date').value = conertSucces1;
-                document.getElementById('_of_rebaselined_rca_creation_target').value = addOneRCA;
+                document.getElementById('rebase_rca_create_target_id').value = conertSucces1;
+                document.getElementById('num_rebase_create_target_id').value = addOneRCA;
             }
             else{
                alert("RCA Terdafat Data Salah")
@@ -142,19 +142,19 @@ function generate() {
   }else{
     if(rca_status != 'Created')
     {
-      if(actual_rca_creation_date === "")
+      if(actual_rca_date_id === "")
       {
          if(date1 > dates2)
          {
-            var addOneRCA  = parseInt(_of_rebaselined_rca_creation_target) + 1;  
+            var addOneRCA  = parseInt(num_rebase_create_target_id) + 1;  
             var convertProcces = new Date(dates2);
             convertProcces.setDate(convertProcces.getDate() + w1);
             var dd = convertProcces.getDate();
             var mm = convertProcces.getMonth() + 1;
             var y = convertProcces.getFullYear();
             var conertSucces1 = y + '-' + mm + '-' + dd;
-            document.getElementById('rebaseline_rca_creation_date').value = conertSucces1;
-            document.getElementById('_of_rebaselined_rca_creation_target').value = addOneRCA;
+            document.getElementById('rebase_rca_create_target_id').value = conertSucces1;
+            document.getElementById('num_rebase_create_target_id').value = addOneRCA;
          }
          else{
              alert("RCAC Belum Bisa Dilakukkan Pada Saat Ini TGL Hari Ini Masih Lebih Kecil Dari Tanggal RCA Creation Date, Lakukan Hari Berikutnya")
@@ -172,28 +172,28 @@ function generate() {
      
      
    //IMP
-var actual_implementation_date                 = (document.getElementById("actual_implementation_date").value);
-var _of_rebaselined_implementation_date_target = (document.getElementById("_of_rebaselined_implementation_date_target").value);
+var act_imp_date_id                 = (document.getElementById("act_imp_date_id").value);
+var num_rebase_imp_date_target_id = (document.getElementById("num_rebase_imp_date_target_id").value);
 var date3  = Date.now(); 
-var date4  = Date.parse(imp_creation_date);
-var dates4 = Date.parse(rebaseline_imp_creation_date);
+var date4  = Date.parse(first_date_target_id);
+var dates4 = Date.parse(rebase_imp_date_target_id);
 
- if(rebaseline_imp_creation_date === ""){  
-   if(actual_implementation_date === "")
+ if(rebase_imp_date_target_id === ""){  
+   if(act_imp_date_id === "")
    {
        if(date3 > date4)
        {
-           if (_of_rebaselined_implementation_date_target < 1)
+           if (num_rebase_imp_date_target_id < 1)
            {
-                var addIMP  = _of_rebaselined_rca_creation_target + 1;   
+                var addIMP  = num_rebase_create_target_id + 1;   
                 var convertProcces = new Date(date4);
                 convertProcces.setDate(convertProcces.getDate() + w2);
                 var dd = convertProcces.getDate();
                 var mm = convertProcces.getMonth() + 1;
                 var y = convertProcces.getFullYear();
                 var conertSucces2 = y + '-' + mm + '-' + dd;
-                document.getElementById('rebaseline_imp_creation_date').value = conertSucces2;
-                document.getElementById('_of_rebaselined_implementation_date_target').value = addIMP;
+                document.getElementById('rebase_imp_date_target_id').value = conertSucces2;
+                document.getElementById('num_rebase_imp_date_target_id').value = addIMP;
            }       
            else{
                alert("IMP Terdafat Data Salah")
@@ -208,33 +208,29 @@ var dates4 = Date.parse(rebaseline_imp_creation_date);
      }
  }
  else{
-    if(actual_implementation_date === "")
+    if(act_imp_date_id === "")
    {
        if(date3 > dates4)
        {
-         var addIMP  = parseInt(_of_rebaselined_rca_creation_target) + 1;   
+         var addIMP  = parseInt(num_rebase_create_target_id) + 1;   
          var convertProcces = new Date(dates4);
          convertProcces.setDate(convertProcces.getDate() + w2);
          var dd = convertProcces.getDate();
          var mm = convertProcces.getMonth() + 1;
          var y = convertProcces.getFullYear();
          var conertSucces2 = y + '-' + mm + '-' + dd;
-         document.getElementById('rebaseline_imp_creation_date').value = conertSucces2;
-         document.getElementById('_of_rebaselined_implementation_date_target').value = addIMP;       
+         document.getElementById('rebase_imp_date_target_id').value = conertSucces2;
+         document.getElementById('num_rebase_imp_date_target_id').value = addIMP;       
         }
         else{
-             alert("IMP Belum Bisa Dilakukkan Pada Saat Ini TGL Hari Ini Masih Lebih Kecil Dari Tanggal First IMP Date Target, Lakukan Hari Berikutnya")
+             alert("IMP Belum Bisa Dilakukkan Pada Saat Ini TGL Hari Ini Masih Lebih Kecil Dari Tanggal First IMP Date Target, Lakukan Hari Berikutnya");
          }
       }
       else{
-          alert("Actual IMP Creation Date Sudah Di Set")
+          alert("Actual IMP Creation Date Sudah Di Set");
      }   
  }
-        
-
-  
-
-  
+       
 }
 
 
@@ -244,7 +240,7 @@ var dates4 = Date.parse(rebaseline_imp_creation_date);
 <h1>Swapping Songs</h1>
 
 <span style="margin-right:1px">Assign Date</span>
-<input id="assign_date" type="text" value="2019-06-11"></br>
+<input id="assigned_date_id" type="text"></br>
 
 <span style="margin-right:1px">Priority</span>
 <select name="priority" id="priority">
@@ -256,25 +252,25 @@ var dates4 = Date.parse(rebaseline_imp_creation_date);
 </select><br/>
 
 <span style="margin-right:1px">RCA Creation Target (otomatis)</span>
-<input id="rca_creation_date" type="text" value="2019-7-23"></br>
+<input id="rca_create_target_id" type="text"></br>
 
 
 <span style="margin-right:1px; color: red">Rebaselined RCA Creation Target (otomatis)</span>
-<input id="rebaseline_rca_creation_date" type="text" value="2019-9-3"></br>
+<input id="rebase_rca_create_target_id" type="text"></br>
 
 <span style="margin-right:1px; color: red">#of Rebaselined RCA Creation Target (otomatis)</span>
-<input id="_of_rebaselined_rca_creation_target" name="_of_rebaselined_rca_creation_target" type="text" value="1"></br>
+<input id="num_rebase_create_target_id" name="num_rebase_create_target_id" type="text"></br>
 
 
 <hr/>
 <span style="margin-right:1px">First Implementation Date Target (otomatis)</span>
-<input id="imp_creation_date"  name="imp_creation_date" type="text" value="2019-9-3"></br>
+<input id="first_date_target_id"  name="first_date_target_id" type="text"></br>
 
 <span style="margin-right:1px; color: red">Rebaselined Implementation Date Target (otomatis)</span>
-<input id="rebaseline_imp_creation_date" type="text" value="2019-11-26"></br>
+<input id="rebase_imp_date_target_id" type="text"></br>
 
 <span style="margin-right:1px; color: red">#of Rebaselined IMP Creation Target (otomatis)</span>
-<input id="_of_rebaselined_implementation_date_target" name="_of_rebaselined_implementation_date_target" type="text" value="1"></br>
+<input id="num_rebase_imp_date_target_id" name="num_rebase_imp_date_target_id" type="text"></br>
 
 
 <br/>
@@ -283,10 +279,10 @@ var dates4 = Date.parse(rebaseline_imp_creation_date);
 <input id="rca_status" name="rca_status" type="text" value="Progress"></br>
 
 <span style="margin-right:1px; color: red">Actual RCA Creation Date</span>
-<input id="actual_rca_creation_date" name="actual_rca_creation_date" type="text"></br>
+<input id="actual_rca_date_id" name="actual_rca_date_id" type="text"></br>
 
 <span style="margin-right:1px; color: red">Actual IMP Creation Date</span>
-<input id="actual_implementation_date" name="actual_implementation_date" type="text"></br>
+<input id="act_imp_date_id" name="act_imp_date_id" type="text"></br>
 
 
 <a href="javascript:void()" onclick="generate();return false;">Generate</a>
